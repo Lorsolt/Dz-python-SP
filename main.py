a596179625265719319615de9b85dedd4e7b0bc1
Lorsolt <mr.lorsolt@mail.ru>
2023-02-22 15:49:22 +0300

    Решение задачи 2

--- main.py
+++ main.py
@@ -39,7 +39,22 @@ else:
 #Если в рейтинге существуют элементы с одинаковыми значениями,
 #то новый элемент с тем же значением должен разместиться после них.
 
-
+number = int(input("Enter number: "))
+my_list = [7, 4, 3, 2]
+c = my_list.count(number)
+for element in my_list:
+    if c > 0:
+        i = my_list.index(number)
+        my_list.insert(i+c, number)
+        break
+    else:
+        if number > element:
+            j = my_list.index(element)
+            my_list.insert(j, number)
+            break
+        elif number < my_list[len(my_list) - 1]:
+            my_list.append(number)
+print(my_list)
 
 #Задание 3. Реализовать структуру данных «Товары». Она должна представлять
 #собой список кортежей.Каждый кортеж хранит информацию об отдельном товаре.
