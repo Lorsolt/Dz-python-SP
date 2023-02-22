dc7c79d31572edefddf9b5732f31f107a863eb20
Lorsolt <mr.lorsolt@mail.ru>
2023-02-22 15:49:42 +0300

    Решение задачи 3

--- main.py
+++ main.py
@@ -65,3 +65,19 @@ print(my_list)
 #словарь,в котором каждый ключ — характеристика товара, например название,
 #а значение — список значений-характеристик, например список названий товаров.
 
+goods = int(input("Введите количество товара "))
+number = 1
+my_dict = []
+my_list = []
+my_analys = {}
+while number <= goods:
+    my_dict = dict({'название': input("введите название "),
+    'цена': input("Введите цену "),'количество': input('Введите количество '),
+    'eд': input("Введите единицу измерения ")})
+    my_list.append((number, my_dict))
+    number += 1
+    my_analys = dict({'название': my_dict.get('название'),
+    'цена': my_dict.get('цена'),'количество': my_dict.get('количество'),
+    'ед': my_dict.get('ед')})
+print(my_list)
+print(my_analys)
